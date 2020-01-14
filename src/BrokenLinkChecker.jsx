bcd1571ea0888fd956f9895edb32da49341ed0da
Coulter Peterson <contact@coulterpeterson.com>
2020-01-13 21:00:04 -0800

    Addon can now console log a recursive list of inks and their broken or non-broken status

--- src/BrokenLinkChecker.jsx
+++ src/BrokenLinkChecker.jsx
@@ -1,30 +1,44 @@
 import React, { Component, Fragment } from 'react';
-import classnames from 'classnames';
-import { ipcRenderer } from 'electron';
-import { confirm } from '@getflywheel/local/renderer';
-import path from 'path';
+const { SiteChecker } = require('broken-link-checker');
 
 export default class BrokenLinkChecker extends Component {
 
-
-
-    render() {
+    componentDidMount() {
 
         let routeChildrenProps = this.props.routeChildrenProps.routeChildrenProps;
         let site = routeChildrenProps.site;
         let siteDomain = site.domain;
         let siteStatus = routeChildrenProps.siteStatus;
 
-        let siteUrl = 'http://' + siteDomain;
-
         // TODO: Add checking to see if site is running with HTTP or HTTPS. Right now HTTP is assumed
         //let possibleSecureHttpStatus = site.services.nginx.ports.HTTP;
         //let otherPossibleSecureHttpStatus = site.services.nginx.role;
 
+        let siteUrl = 'http://' + siteDomain;
+
+        this.checkLinks(siteUrl);
+
+    }
+
+    checkLinks(siteURL) {
+        let siteChecker = new SiteChecker(null, {
+            robots: function (robots, customData) { },
+            html: function (tree, robots, response, pageUrl, customData) { },
+            junk: function (result, customData) { },
+            link: function (result, customData) { console.log(result) },
+            page: function (error, pageUrl, customData) { },
+            site: function (error, siteUrl, customData) { },
+            end: function () { }
+        });
+        siteChecker.enqueue(siteURL);
+    }
+
+
+    render() {
 
         return (
             <div style={{ flex: '1', overflowY: 'auto' }}>
-                <h2>{siteUrl}</h2>
+                <h2>Hello World</h2>
             </div>
         )
     }
