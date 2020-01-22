d005d1ab97895e276029d3dcafca32d08d13bb34
Coulter Peterson <coulter.peterson@p2c.com>
2020-01-22 12:15:45 -0800

    Getting checker back to working order since we are not storing the data properly yet.

--- src/BrokenLinkChecker.jsx
+++ src/BrokenLinkChecker.jsx
@@ -1,129 +1,135 @@
-import React, { Component, Fragment } from 'react';
-import { ipcRenderer } from 'electron';
-const { SiteChecker, HtmlUrlChecker } = require('broken-link-checker');
+import React, { Component, Fragment } from "react";
+import { ipcRenderer } from "electron";
+const { SiteChecker, HtmlUrlChecker } = require("broken-link-checker");
 
 export default class BrokenLinkChecker extends Component {
+	constructor(props) {
+		super(props);
 
-    constructor(props) {
+		this.state = {
+			brokenLinks: []
+		};
 
-        super(props);
+		this.checkLinks = this.checkLinks.bind(this);
+	}
 
-        this.state = {
-            brokenLinks: [],
-        };
+	componentDidMount() {}
 
-        this.checkLinks = this.checkLinks.bind(this);
-    }
+	addBrokenLink(statusCode, linkURL, linkText, originURL, wpPostId) {
+		let newBrokenLink = {
+			statusCode: statusCode,
+			linkURL: linkURL,
+			linkText: linkText,
+			originURL: originURL,
+			wpPostId: wpPostId
+		};
 
-    componentDidMount() { }
+		this.setState(prevState => ({
+			brokenLinks: [...prevState.brokenLinks, newBrokenLink]
+		}));
+		// ,this.syncBrokenLinksToSite
+	}
 
-    addBrokenLink(statusCode, linkURL, linkText, originURL, wpPostId) {
-        let newBrokenLink = {
-            "statusCode": statusCode,
-            "linkURL": linkURL,
-            "linkText": linkText,
-            "originURL": originURL,
-            "wpPostId": wpPostId
-        };
+	syncBrokenLinksToSite() {
+		// TODO: get access to the 'site' object
+		//ipcRenderer.send('store-broken-links', this.props.site.id, this.state.brokenLinks);
+	}
 
-        this.setState(prevState => ({
-            brokenLinks: [...prevState.brokenLinks, newBrokenLink]
-        }, this.syncBrokenLinksToSite));
-    }
+	// TODO get access to 'site' so that I can pull down the broken links and confirm that they are being saved properly
+	// fetchBrokenLinks() {
 
-    componentDidUpdate(previousProps) {
-        console.log(this.props);
-    }
+	// 	const notes = this.props.site.notes;
 
-    syncBrokenLinksToSite() {
-        ipcRenderer.send('store-broken-links', this.props.site.id, this.state.brokenLinks);
-    }
+	// 	if (!notes) {
+	// 		return [];
+	// 	}
 
-    // TODO get access to 'site' so that I can pull down the broken links and confirm that they are being saved properly
-    // fetchBrokenLinks() {
+	// 	for (const [noteIndex, note] of notes.entries()) {
+	// 		if (note.date instanceof Date || !note.date) {
+	// 			continue;
+	// 		}
 
-    // 	const notes = this.props.site.notes;
+	// 		notes[noteIndex].date = new Date(note.date);
+	// 	}
 
-    // 	if (!notes) {
-    // 		return [];
-    // 	}
+	// 	return notes;
+	// }
 
-    // 	for (const [noteIndex, note] of notes.entries()) {
-    // 		if (note.date instanceof Date || !note.date) {
-    // 			continue;
-    // 		}
+	startScan = () => {
+		let routeChildrenProps = this.props.routeChildrenProps
+			.routeChildrenProps;
+		let site = routeChildrenProps.site;
+		let siteDomain = site.domain;
+		let siteStatus = routeChildrenProps.siteStatus;
 
-    // 		notes[noteIndex].date = new Date(note.date);
-    // 	}
+		// TODO: Add checking to see if site is running with HTTP or HTTPS. Right now HTTP is assumed
+		//let possibleSecureHttpStatus = site.services.nginx.ports.HTTP;
+		//let otherPossibleSecureHttpStatus = site.services.nginx.role;
 
-    // 	return notes;
-    // }
+		let siteUrl = "http://" + siteDomain;
 
-    startScan = () => {
-        let routeChildrenProps = this.props.routeChildrenProps.routeChildrenProps;
-        let site = routeChildrenProps.site;
-        let siteDomain = site.domain;
-        let siteStatus = routeChildrenProps.siteStatus;
+		this.checkLinks(siteUrl);
+	};
 
-        // TODO: Add checking to see if site is running with HTTP or HTTPS. Right now HTTP is assumed
-        //let possibleSecureHttpStatus = site.services.nginx.ports.HTTP;
-        //let otherPossibleSecureHttpStatus = site.services.nginx.role;
+	checkLinks(siteURL) {
+		let siteChecker = new SiteChecker(null, {
+			link: (result, customData) => {
+				if (result.broken) {
+					let brokenLinkScanResults = {
+						statusCode: String(result.http.response.statusCode),
+						linkURL: String(result.url.original),
+						linkText: String(result.html.text),
+						originURL: String(result.base.original)
+					};
 
-        let siteUrl = 'http://' + siteDomain;
+					let singlePageChecker = new HtmlUrlChecker(null, {
+						html: (tree, robots, response, pageUrl, customData) => {
+							// TODO: Make this code continue to drill down until an exact match for the 'body' tag is found, just in case a custom template has modified the usual page structure
+							let stringOfBodyClasses =
+								tree.childNodes[1].childNodes[2].attrMap.class;
 
-        this.checkLinks(siteUrl);
-    }
+							// TODO: Also make note of special classes like .home
+							let findPostId = stringOfBodyClasses.match(
+								/(^|\s)postid-(\d+)(\s|$)/
+							);
 
-    checkLinks(siteURL) {
-        let siteChecker = new SiteChecker(null, {
-            link: (result, customData) => {
-                if (result.broken) {
+							let wpPostId = null;
+							if (findPostId) {
+								wpPostId = findPostId[2];
+							}
 
-                    let brokenLinkScanResults = {
-                        "statusCode": String(result.http.response.statusCode),
-                        "linkURL": String(result.url.original),
-                        "linkText": String(result.html.text),
-                        "originURL": String(result.base.original)
-                    }
+							this.addBrokenLink(
+								customData["statusCode"],
+								customData["linkURL"],
+								customData["linkText"],
+								customData["originURL"],
+								wpPostId
+							);
+						}
+					});
+					singlePageChecker.enqueue(
+						brokenLinkScanResults["originURL"],
+						brokenLinkScanResults
+					);
+				}
+			}
+		});
+		siteChecker.enqueue(siteURL);
+	}
 
-                    let singlePageChecker = new HtmlUrlChecker(null, {
-                        html: (tree, robots, response, pageUrl, customData) => {
-                            // TODO: Make this code continue to drill down until an exact match for the 'body' tag is found, just in case a custom template has modified the usual page structure
-                            let stringOfBodyClasses = tree.childNodes[1].childNodes[2].attrMap.class;
+	render() {
+		console.log(this.state.brokenLinks);
 
-                            // TODO: Also make note of special classes like .home
-                            let findPostId = stringOfBodyClasses.match(/(^|\s)postid-(\d+)(\s|$)/);
-
-                            let wpPostId = null;
-                            if (findPostId) {
-                                wpPostId = findPostId[2];
-                            }
-
-                            this.addBrokenLink(customData["statusCode"], customData["linkURL"], customData["linkText"], customData["originURL"], wpPostId);
-                        }
-                    });
-                    singlePageChecker.enqueue(brokenLinkScanResults["originURL"], brokenLinkScanResults);
-                }
-            }
-        });
-        siteChecker.enqueue(siteURL);
-    }
-
-    render() {
-
-        return (
-            <div style={{ flex: '1', overflowY: 'auto' }}>
-                <h2>Behold the links:</h2>
-                <ul>
-                    {this.state.brokenLinks.map(item => (
-                        <li key={item["linkURL"]}>{item["linkURL"]}</li>
-                    ))}
-                </ul>
-                <p onClick={this.startScan}>
-                    Start Scan
-                </p>
-            </div>
-        )
-    }
-
+		return (
+			<div style={{ flex: "1", overflowY: "auto" }}>
+				<h2>Behold the links:</h2>
+				<ul>
+					{this.state.brokenLinks.map(item => (
+						<li key={item["linkURL"]}>{item["linkURL"]}</li>
+					))}
+				</ul>
+				<p onClick={this.startScan}>Start Scan</p>
+			</div>
+		);
+	}
 }
