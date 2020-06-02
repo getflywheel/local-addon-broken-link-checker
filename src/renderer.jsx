53ae7fa1c736bb98adb78dc9009e07805f6a8b5a
coulterpeterson <coulter@maplespaceinc.com>
2020-06-02 10:48:15 -0700

    Updated styles for wider header display

--- src/renderer.jsx
+++ src/renderer.jsx
@@ -6,7 +6,7 @@ import { matchPath } from "react-router";
 export default function(context) {
 	const { React, hooks } = context;
 	const { Route } = context.ReactRouter;
-	const stylesheetPath = path.resolve("file://", __dirname, "./style.BrokenLinkChecker.css");
+	const stylesheetPath = path.resolve(__dirname, "../style.css");
 
 	hooks.addContent("stylesheets", () => (
 		<link
