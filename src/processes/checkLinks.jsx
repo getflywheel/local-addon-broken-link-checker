a884e52299e272663db127b3f93507b92c6ef0d0
coulterpeterson <coulter@maplespaceinc.com>
2020-07-07 11:21:42 -0700

    Using promises for easier process responses

--- src/processes/checkLinks.jsx
+++ src/processes/checkLinks.jsx
@@ -8,7 +8,10 @@ const {
 
 // receive message from master process
 process.on('message', (m) => {
-	process.send(m);
+	
+	if( (m[0] === "start-scan") && (m[1] !== 'undefined')) {
+		checkLinks(m[1]).then((data) => process.send(["scan-finished", data]));
+	}
 	
 	// This commented-out code seems to prevent a reponse from being sent
 	// if( (m[0] === "start-scan") && (m[1] !== 'undefined')) {
@@ -27,75 +30,85 @@ process.on('message', (m) => {
 	// }
   });
 
-async function checkLinks(siteURL) {   
+let checkLinks = function(siteURL) {
+	return new Promise(function(resolve, reject) {
 
-	addBrokenLink("1" ,"1", "1", "1", "1");
-	return "cheese";
-
-  	let options = new Object();
-	options.maxSocketsPerHost = 15;
-
-	let siteChecker = new SiteChecker(options, {
-		html: (tree, robots, response, pageUrl, customData) => {
-			// This code is used to increment the number of WP posts we traverse in our scan
-			if (findWpPostIdInMarkup(tree)) {
-				incrementNumberPostsFound();
-			}
-		},
-		link: (result, customData) => {
-			if (result.broken) {
-				let brokenLinkScanResults = {
-					statusCode: String(result.http.response.statusCode),
-					linkURL: String(result.url.original),
-					linkText: String(result.html.text),
-					originURL: String(result.base.original),
-					originURI: String(result.base.parsed.path),
-					resultDump: result
-				};
-
-				let singlePageChecker = new HtmlUrlChecker(null, {
-					html: (tree, robots, response, pageUrl, customData) => {
-
-						let wpPostId = findWpPostIdInMarkup(tree);
-
-						if (wpPostId !== null) {
-							addBrokenLink(
-								customData["statusCode"],
-								customData["linkURL"],
-								customData["linkText"],
-								customData["originURL"],
-								customData["originURI"],
-								wpPostId
-							);
-
-							updateBrokenLinksFound(true);
-						}
-					}
-				});
-
-				singlePageChecker.enqueue(
-					brokenLinkScanResults["originURL"],
-					brokenLinkScanResults
-				);
-			}
-		},
-		end: (result, customData) => {
-			// At last the first run is done, so we update the state
-			updateFirstRunComplete(true);
-			updateScanInProgress(false);
-
-			if (
-				this.state.brokenLinks === null ||
-				this.state.brokenLinks.length === 0
-			) {
-				updateBrokenLinksFound(false);
-			}
-		},
+		if(true){
+			resolve("Stuff worked!");
+		} else {
+			reject(Error("It broke"));
+		}
+		
 	});
-	siteChecker.enqueue(siteURL);
+} 
 
-    return "hello";
-}
+// 	//addBrokenLink("1" ,"1", "1", "1", "1");
+// 	return "cheese";
+
+//   	let options = new Object();
+// 	options.maxSocketsPerHost = 15;
+
+// 	let siteChecker = new SiteChecker(options, {
+// 		html: (tree, robots, response, pageUrl, customData) => {
+// 			// This code is used to increment the number of WP posts we traverse in our scan
+// 			if (findWpPostIdInMarkup(tree)) {
+// 				incrementNumberPostsFound();
+// 			}
+// 		},
+// 		link: (result, customData) => {
+// 			if (result.broken) {
+// 				let brokenLinkScanResults = {
+// 					statusCode: String(result.http.response.statusCode),
+// 					linkURL: String(result.url.original),
+// 					linkText: String(result.html.text),
+// 					originURL: String(result.base.original),
+// 					originURI: String(result.base.parsed.path),
+// 					resultDump: result
+// 				};
+
+// 				let singlePageChecker = new HtmlUrlChecker(null, {
+// 					html: (tree, robots, response, pageUrl, customData) => {
+
+// 						let wpPostId = findWpPostIdInMarkup(tree);
+
+// 						if (wpPostId !== null) {
+// 							addBrokenLink(
+// 								customData["statusCode"],
+// 								customData["linkURL"],
+// 								customData["linkText"],
+// 								customData["originURL"],
+// 								customData["originURI"],
+// 								wpPostId
+// 							);
+
+// 							updateBrokenLinksFound(true);
+// 						}
+// 					}
+// 				});
+
+// 				singlePageChecker.enqueue(
+// 					brokenLinkScanResults["originURL"],
+// 					brokenLinkScanResults
+// 				);
+// 			}
+// 		},
+// 		end: (result, customData) => {
+// 			// At last the first run is done, so we update the state
+// 			updateFirstRunComplete(true);
+// 			updateScanInProgress(false);
+
+// 			if (
+// 				this.state.brokenLinks === null ||
+// 				this.state.brokenLinks.length === 0
+// 			) {
+// 				updateBrokenLinksFound(false);
+// 			}
+// 		},
+// 	});
+// 	siteChecker.enqueue(siteURL);
+
+//     return "hello";
+// }
 
 
 function findWpPostIdInMarkup(tree) {
