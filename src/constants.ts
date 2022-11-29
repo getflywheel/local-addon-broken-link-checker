f55bad2873f41cff9e985ae6c8cda2d327b2e8ed
Danni Douglas <danni.douglas@wpengine.com>
2022-11-29 14:25:32 -0600

    chore: some cleanup, linting, and making some text adjustments

--- src/constants.ts
+++ src/constants.ts
@@ -1,35 +1,36 @@
 const SCAN_USER_AGENT = Object.freeze({
-	DEFAULT: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
+	DEFAULT:
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36",
 });
 
 const TABLE_HEADERS = {
-	STATUS : {
-		KEY: 'statusCode',
-		TEXT: 'Status',
+	STATUS: {
+		KEY: "statusCode",
+		TEXT: "Status",
 	},
 	ORIGIN_URL: {
-		KEY:'originURL',
-		TEXT: 'Origin URL'
+		KEY: "originURL",
+		TEXT: "Origin URL",
 	},
 	ORIGIN_URI: {
-		KEY:'originURI',
-		TEXT: 'Origin URI'
+		KEY: "originURI",
+		TEXT: "Origin URI",
 	},
 	LINK_URL: {
-		KEY: 'linkURL',
-		TEXT: 'Link URL'
+		KEY: "linkURL",
+		TEXT: "Link URL",
 	},
 	LINK_TEXT: {
-		KEY: 'linkText',
-		TEXT: 'Link Text',
+		KEY: "linkText",
+		TEXT: "Link text",
 	},
 	FILL: {
-		KEY: 'fill',
-		TEXT: ''
-	}
-}
+		KEY: "fill",
+		TEXT: "",
+	},
+};
 
 module.exports = {
 	SCAN_USER_AGENT,
 	TABLE_HEADERS,
-}
+};
